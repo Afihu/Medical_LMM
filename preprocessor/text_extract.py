eba9115252047f34c296bc3dca00a1f6ce6191f9
ThanhLe86 <135790325+ThanhLe86@users.noreply.github.com>
2025-10-14 22:03:19 +0700

    added: rough text data cleaning

--- preprocessor/text_extract.py
+++ preprocessor/text_extract.py
@@ -1,4 +1,5 @@
 import fitz  # import  PyMuPDF library
+import re
 
 try:
     pdf_path = "source_materials/1---A-20-Year-Old-Woman-from-Sudan-With-Fever--_2022_Clinical-Cases-in-Tropi.pdf"
@@ -11,14 +12,19 @@ try:
     for page_num in range(doc.page_count):
         page = doc.load_page(page_num)  
         text = page.get_text("text")   # Extract text from the page
-        
-        full_text += f"--- Page {page_num + 1} ---\n"
         full_text += text + "\n"
+    
+    # cleaning
+    phrase = "Further Reading"
+    parts = full_text.split(phrase)
+    cleaned = parts[0]
+    cleaned = re.sub(r'• Figure|Fig\.\s\d+\.\d+\s*', '', cleaned)
+
 
     # save to file
     output_text_file = "preprocessor/res/extracted_text.txt"
     with open(output_text_file, "w", encoding="utf-8") as f:
-        f.write(full_text)
+        f.write(cleaned.lstrip())
         
     print(f"Successfully extracted text to '{output_text_file}'")
 
