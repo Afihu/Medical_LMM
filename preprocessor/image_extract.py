11e5b88d3e8bab9940a8d88c1fa23ed0dc5ab99e
ThanhLe86 <135790325+ThanhLe86@users.noreply.github.com>
2025-10-14 22:31:03 +0700

    added: extract image dimensions

--- preprocessor/image_extract.py
+++ preprocessor/image_extract.py
@@ -23,12 +23,24 @@ try:
         for img_index, img_info in enumerate(image_list):
             xref = img_info[0]
             
+            # code for extracting image dimensions
+            try:
+                pix = fitz.Pixmap(doc, xref)
+                width = pix.width
+                height = pix.height
+                print(f"Dimensions: {width}x{height}")
+                pix = None
+            except Exception as e:
+                print(f"Error processing image with XREF {xref}: {e}")
+
+
             # Extract the raw image data using the xref
             base_image = doc.extract_image(xref)
             image_bytes = base_image["image"]
             image_ext = base_image["ext"]
             
             image_filename = f"page{page_num+1}_img{img_index+1}.{image_ext}"
+            print(image_filename)
             image_path = os.path.join(output_dir, image_filename)
             
             with open(image_path, "wb") as img_file:
