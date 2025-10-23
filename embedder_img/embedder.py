129cec42f32ed1e6fd0f1397b774b2717e1d7b62
ThanhLe86 <135790325+ThanhLe86@users.noreply.github.com>
2025-10-23 22:27:33 +0700

    completed: image-embedder

--- embedder_img/embedder.py
+++ embedder_img/embedder.py
@@ -1,44 +1,40 @@
-import numpy as np
+# import numpy as np
 from PIL import Image
 from transformers import AutoProcessor, AutoModel
-from tensorflow.image import resize as tf_resize
+# from tensorflow.image import resize as tf_resize
 import torch
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {device}") # <-- Add this line
 
 model = AutoModel.from_pretrained("google/medsiglip-448").to(device)
 processor = AutoProcessor.from_pretrained("google/medsiglip-448")
 
 image_path = "embedder_img/img/page2_img1.jpeg"
 
-img = Image.open(image_path)
+img = [Image.open(image_path).convert("RGB")]
 
-def resize(image):
-    return Image.fromarray(
-        tf_resize(
-            images=image, size=[448, 448], method='bilinear', antialias=False
-        ).numpy().astype(np.uint8)
-    )
+# def resize(image):
+#     return Image.fromarray(
+#         tf_resize(
+#             images=image, size=[448, 448], method='bilinear', antialias=False
+#         ).numpy().astype(np.uint8)
+#     )
 
 
-resized_imgs = [resize(img)]
+# resized_imgs = [resize(img)]
 
 texts = [
-    "Oral bleeding in Ebola virus disease. (Bausch, D.G., 2008. Viral hemorrhagic fevers. In: Schlossberg, D. (Ed.), Clinical Infectious Disease. Cambridge University Press, New York. Used with permission. Photo by Bausch, D.)",
+    "Oral bleeding in Ebola virus disease. (Bausch, D.G., 2008. Viral hemorrhagic fevers. In: Schlossberg, D. (Ed.), Clinical Infectious Disease. Cambridge University Press, New York. Used with permission. Photo by Bausch, D.)"
 ]
 
-inputs = processor(text=texts, images=resized_imgs, padding="max_length", return_tensors="pt").to(device)
+inputs = processor(text=texts, images=img, padding="max_length", return_tensors="pt").to(device)
 
 with torch.no_grad():
     outputs = model(**inputs)
 
 logits_per_image = outputs.logits_per_image
 probs = torch.softmax(logits_per_image, dim=1)
-
-for n_img, img in enumerate(imgs):
-    display(img)  # Note this is an IPython function that will only work in a Jupyter notebook environment
-    for i, label in enumerate(texts):
-        print(f"{probs[n_img][i]:.2%} that image is '{label}'")
 
 # Get the image and text embeddings
 print(f"image embeddings: {outputs.image_embeds}")
