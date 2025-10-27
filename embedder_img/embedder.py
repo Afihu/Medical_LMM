73268ddffa9a6d4682490e1c57bdcfd35ee532e3
ThanhLe86 <135790325+ThanhLe86@users.noreply.github.com>
2025-10-27 17:32:38 +0700

    added: requested dedicated script

--- embedder_img/embedder.py
+++ embedder_img/embedder.py
@@ -7,26 +7,6 @@ import torch
 # from preprocessor import text_extract
 # import numpy as np
 
-def embed_img(image_path, texts):
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print(f"Using device: {device}") # <-- Add this line
-
-    model = AutoModel.from_pretrained("google/medsiglip-448").to(device)
-    processor = AutoProcessor.from_pretrained("google/medsiglip-448")
-
-    img = [Image.open(image_path).convert("RGB")]
-
-    inputs = processor(text=texts, images=img, padding="max_length", return_tensors="pt").to(device)
-
-    with torch.no_grad():
-        outputs = model(**inputs)
-
-    logits_per_image = outputs.logits_per_image
-    probs = torch.softmax(logits_per_image, dim=1)
-    
-    return outputs
-
 
 ### Embedder function of interest
 ### The main embedding mechanism will go through each PDF to extract images and captions, store in arrays, 
