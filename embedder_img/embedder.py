134f5811abf366caf4fcddb776db49ee2b2ef9eb
ThanhLe86 <135790325+ThanhLe86@users.noreply.github.com>
2025-10-27 10:37:55 +0700

    refactor: embedder script

--- embedder_img/embedder.py
+++ embedder_img/embedder.py
@@ -3,31 +3,29 @@ from PIL import Image
 from transformers import AutoProcessor, AutoModel
 import torch
 
-# Debugging import
-from preprocessor import text_extract
-import numpy as np
+# # Debugging import
+# from preprocessor import text_extract
+# import numpy as np
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using device: {device}") # <-- Add this line
+def embed_img(image_path, texts):
 
-model = AutoModel.from_pretrained("google/medsiglip-448").to(device)
-processor = AutoProcessor.from_pretrained("google/medsiglip-448")
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(f"Using device: {device}") # <-- Add this line
 
-image_path = "embedder_img/img/page2_img1.jpeg"
+    model = AutoModel.from_pretrained("google/medsiglip-448").to(device)
+    processor = AutoProcessor.from_pretrained("google/medsiglip-448")
 
-img = [Image.open(image_path).convert("RGB")]
+    img = [Image.open(image_path).convert("RGB")]
 
-texts = [
-    "Oral bleeding in Ebola virus disease. (Bausch, D.G., 2008. Viral hemorrhagic fevers. In: Schlossberg, D. (Ed.), Clinical Infectious Disease. Cambridge University Press, New York. Used with permission. Photo by Bausch, D.)"
-]
+    inputs = processor(text=texts, images=img, padding="max_length", return_tensors="pt").to(device)
 
-inputs = processor(text=texts, images=img, padding="max_length", return_tensors="pt").to(device)
+    with torch.no_grad():
+        outputs = model(**inputs)
 
-with torch.no_grad():
-    outputs = model(**inputs)
-
-logits_per_image = outputs.logits_per_image
-probs = torch.softmax(logits_per_image, dim=1)
+    logits_per_image = outputs.logits_per_image
+    probs = torch.softmax(logits_per_image, dim=1)
+    
+    return outputs
 
 
 # #### debugging
