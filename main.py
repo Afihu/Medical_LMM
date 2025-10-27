8d72b2293697ba44f1430b526032cadff0cbd12a
ThanhLe86 <135790325+ThanhLe86@users.noreply.github.com>
2025-10-27 09:08:08 +0700

    improved: caption extraction engine

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from preprocessor import caption_extract
+from preprocessor import caption_extract, text_extract
 
 def main():
     print("Init texting external modules")
 
-    pdf_path = "source_materials/3---A-26-Year-Old-Woman-from-Malawi-with-Headache-_2022_Clinical-Cases-in-Tr.pdf"
+    pdf_path = "source_materials/2---A-7-Year-Old-Girl-from-Peru-With-a-Chron_2022_Clinical-Cases-in-Tropical.pdf"
 
-    caption_extract.extract_captions(pdf_path)
+    pdf_texts = text_extract.extract_text(pdf_path)
+    captions = caption_extract.extract_captions(pdf_texts)
+    
+    for caption in captions: 
+        print(caption)
 
     
 
--- preprocessor/caption_extract.py
+++ preprocessor/caption_extract.py
@@ -1,34 +1,15 @@
-import fitz  # import  PyMuPDF library
 import re
 
-def extract_captions(pdf_path):
-    try:
-        doc = fitz.open(pdf_path)
+def extract_captions(text_doc):
 
-        print(f"--- Searching for captions ---")
-        found_captions = []
-        
-        for page_num, page in enumerate(doc):
-            blocks = page.get_text("blocks")
+    captions  = []
+    patterns = r"^(•\s*)?(fig|figure|table)\.?\s*\d"
 
-            # Filter for text blocks (type == 0)
-            text_blocks = [b for b in blocks if b[6] == 0]
+    print(f"--- Searching for captions ---")
 
-            for block in text_blocks:
-                text = block[4].strip()
-                if re.match(r"(•\s*)?(fig|figure|table)\.?\s*\d", text, re.IGNORECASE):
-                
-                    # Found one! Clean up newlines for printing.
-                    clean_caption = text.replace('\n', ' ')
-                    print(f"  [Found on Page {page_num + 1}]: {clean_caption}")
-                    found_captions.append(clean_caption)
+    for line in text_doc.splitlines():
+        clean_line = line.strip()
+        if re.search(patterns, clean_line, re.IGNORECASE):
+            captions.append(clean_line)
 
-        if not found_captions:
-            print("No captions were found.")
-            
-        doc.close()
-
-    except FileNotFoundError:
-        print(f"Error: The file '{pdf_path}' was not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    return captions
