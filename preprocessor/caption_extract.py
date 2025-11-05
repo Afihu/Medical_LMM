2b7479073ba2a525b0dbcb7d1b2a1d3d7cb73c05
ThanhLe86 <135790325+ThanhLe86@users.noreply.github.com>
2025-11-05 15:32:05 +0700

    added: rudimentary keyword extractor

--- preprocessor/caption_extract.py
+++ preprocessor/caption_extract.py
@@ -1,4 +1,5 @@
 import re
+import yake
 # from preprocessor import text_extract
 
 def extract_captions(text_doc):
@@ -43,6 +44,24 @@ def extract_captions(text_doc):
 
     return captions
 
+# Run pip install yake before running this code
+def extract_keyword(text_doc):
+    kw_extractor = yake.KeywordExtractor(
+        lan = "en",
+        n=5,
+        dedupLim=0.9,
+        top=15
+    )
+
+    keywords = kw_extractor.extract_keywords(text_doc)
+
+    print("keywords: \n")
+    count = 0
+    for kw, score in keywords:
+        print(kw)
+
+
+
 # if __name__ == "__main__":
 #     text_doc = text_extract.extract_text("source_materials/2---A-7-Year-Old-Girl-from-Peru-With-a-Chron_2022_Clinical-Cases-in-Tropical.pdf")
 #     text_extract.txt_print(text_doc)
@@ -50,3 +69,9 @@ def extract_captions(text_doc):
 #     print(len(captions))
 #     for caption in captions:
 #         print(caption, "\n\n")
+
+if __name__ == "__main__":
+    text = """
+        Ms. Anya Sharma, a 28-year-old software engineer, presented to the clinic with an acute onset of high fever, severe retro-orbital pain, and debilitating joint and muscle pain, often referred to as "breakbone fever." She returned four days ago from a week-long vacation in Southeast Asia where she reported frequent daytime mosquito bites. Initial blood work reveals leukopenia and progressive thrombocytopenia (platelets currently 90,000/μL), raising strong clinical suspicion for Dengue Fever. The patient is currently being monitored for any warning signs of severe plasma leakage or bleeding complications, requiring close management of fluid balance.
+    """
+    extract_keyword(text)
